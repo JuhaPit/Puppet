81da11d35aeadb4bcc7829df3e56f5ade7a3a598
Juha Pitkänen <jwpitkanen@gmail.com>
2017-11-07 19:06:34 +0200

    Fixed typo

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Setting keyboard to Finnish layout"
+echo "---Setting keyboard to Finnish layout---"
 setxkbmap fi
 
-echo "Updating package repositories"
+echo "---Updating package repositories---"
 sudo apt-get update
 
-echo "Installing Git and Puppet"
+echo "---Installing Git and Puppet---"
 sudo apt-get install -y git puppet
 
-echo "Creating Git-folder to users home directory and pulling repository"
+echo "---Creating Git-folder to users home directory and pulling repository---"
 cd /home/$(whoami)/
 mkdir git
 cd git
 git clone https://github.com/JuhaPit/Puppet.git
 
-echo "Replacing Puppet's default content in /etc/puppet with content from repository"
-sudo cp -TRv ./puppet /etc/puppet
+echo "---Replacing Puppet's default content in /etc/puppet with content from repository---"
+sudo cp -TRv ./Puppet /etc/puppet
 
-echo "Running Puppet's site.pp, installing SSH daemon"
+echo "---Running Puppet's site.pp, installing SSH daemon---"
 sudo puppet apply /etc/puppet/manifests/site.pp
