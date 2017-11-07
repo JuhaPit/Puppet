b078d5fb91919166b922757bcc5b1d4383a3d464
Juha Pitkänen <jwpitkanen@gmail.com>
2017-11-07 18:54:01 +0200

    Scripts to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-#My first script
-echo "Hello World!"
+echo "Setting keyboard to Finnish layout"
+setxkbmap fi
+
+echo "Updating package repositories"
+sudo apt-get update
+
+echo "Installing Git and Puppet"
+sudo apt-get install -y git puppet
+
+echo "Creating Git-folder to users home directory and pulling repository"
+cd /home/$(whoami)/
+mkdir git
+cd git
+git clone https://github.com/JuhaPit/Puppet.git
+
+echo "Replacing Puppet's default content in /etc/puppet with content from repository"
+sudo cp -TRv ./puppet /etc/puppet
+
+echo "Running Puppet's site.pp, installing SSH daemon"
+sudo puppet apply /etc/puppet/manifests/site.pp
